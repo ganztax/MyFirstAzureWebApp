07afcbd621a8eff99eed9a2ad75f190d5d3fac8e
Karl Danielsson <karl.d98@hotmail.com>
2026-03-20 09:20:07 +0100

    serets removed from commit message template

--- obj/Debug/net10.0/MyFirstAzureWebApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyFirstAzureWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa10ff35df9a22ba6927168db6ab0adc4b12199")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b7b3a6cd727e79e919bfec81c14e540bc926a47")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyFirstAzureWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
